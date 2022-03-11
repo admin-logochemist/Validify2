0b035048cc2c9bddc96338e972ec821ba7429460
admin-logochemist <team@logochemist.com>
2022-03-11 00:21:47 -0700

    hamza khan

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -4,253 +4,106 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Trades() {
   return (
-
-    <div className="col_iframe scd_width">
-    <h3>Trades</h3>
-    <div class="tbl-header">
-        <table className='table'>
-        <thead>
-            <tr>
-            <th>Date</th>
-            <th>Type</th>
-            <th>Price USD</th>
-            <th>Amount/UTP</th>
-            <th>Token Price</th>
-            </tr>
-        </thead>
-        </table>
-    </div>
-    <div class="tbl-content">
+    <div className='table_trades mt-5 mb-3'>
+            <div className='flex_box_table'>
+               <h3>Trades</h3>
+               <button className='buy_btn'>Buy</button>
+               <button className='sale_btn'>Sale</button>
+            </div>
         <table>
-        <tbody>
-            <tr>
-            <td>ADD</td>
-            <td className='buy_color'>buy</td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>ADD</td>
-            <td className='sale_color'>sale</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='buy_color'>buy</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='sale_color'>sale</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='buy_color'>buy</td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>AAD</td>
-            <td className='sale_color'>sale</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='buy_color'>buy</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='sale_color'>sale</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='buy_color'>buy </td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>AAD</td>
-            <td className='sale_color'>sale</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='buy_color'>buy</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='sale_color'>sale</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='buy_color'>buy</td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>AAD</td>
-            <td className='buy_color'>buy</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='sale_color'>sale</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='buy_color'></td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='sale_color'>sale</td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>AAD</td>
-            <td className='buy_color'>AUSENCO</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='sale_color'>sale</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='buy_color'>ADITYA BIRLA</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='sale_color'>sale</td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>AAD</td>
-            <td className='buy_color'>AUSENCO</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='sale_color'>sale</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='buy_color'>ADITYA BIRLA</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='sale_color'>sale</td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td >AAD</td>
-            <td className='buy_color'>AUSENCO</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='sale_color'>sale</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td>XXD</td>
-            <td className='buy_color'>ADITYA BIRLA</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-            <tr>
-            <td>AAC</td>
-            <td className='sale_color'>sale </td>
-            <td>$1.38</td>
-            <td>+2.01</td>
-            <td>-0.36%</td>
-            </tr>
-            <tr>
-            <td>AAD</td>
-            <td className='buy_color'>AUSENCO</td>
-            <td>$2.38</td>
-            <td>-0.01</td>
-            <td>-1.36%</td>
-            </tr>
-            <tr>
-            <td>AAX</td>
-            <td className='sale_color'>sale</td>
-            <td>$3.22</td>
-            <td>+0.01</td>
-            <td>+1.36%</td>
-            </tr>
-            <tr>
-            <td className='buy_color'>XXD</td>
-            <td>ADITYA BIRLA</td>
-            <td>$1.02</td>
-            <td>-1.01</td>
-            <td>+2.36%</td>
-            </tr>
-        </tbody>
+            <thead>
+                <th>Date</th>
+                <th>Token</th>
+                <th>Type</th>
+                <th>Price USD</th>
+                <th>Amount UTP</th>
+                <th>Token Price</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+                <tr>
+                    <td>13-Jan</td>
+                    <td>USDT</td>
+                    <td className='buy'>Buy</td>
+                    <td>$345.87</td>
+                    <td>6014.54</td>
+                    <td>$1.004</td>
+                </tr>
+
+            </tbody>
         </table>
     </div>
-</div>
   )
 }
 
