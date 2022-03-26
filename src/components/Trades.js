9ec05c27cf144acfa0be02e3337b965f6dc616d9
Taichee Rixvi <85700339+admin-logochemist@users.noreply.github.com>
2022-03-26 22:31:16 +0500

    Ghayas

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,17 +1,27 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import '../css/trades.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import moment from "moment";
 import { BscScan } from "@jpmonette/bscscan";
 function Trades() {
+    const[resf,setRes]=useState([]);
+    const callApi=()=>{
+        fetch('http://localhost:9000/users?currency=ETH/USDT').then((res)=>res.json().then(re=>{setRes(re)
+    
+    }))
+    }
+
+    useEffect(() => {
+        callApi()
+    }, [])
 
     const client = new BscScan({ apikey: "4M737ZIVF6RDFH1E4FMX7HBGWBV4ZVRJKI" });
 
     const balance =  client.accounts.getBalance({ address: "0x4e656459ed25bf986eea1196bc1b00665401645d" });
-    console.log("balance",balance)
+   
     const txlist = client.accounts.getTxList({ address: "0x4e656459ed25bf986eea1196bc1b00665401645d" });
         txlist.then((data)=>{
-        console.log( "ham",data);
+       
         setTrades(data)
         }
      )
@@ -20,7 +30,21 @@ function Trades() {
  
   return (
     <div className='table_trades'>
-        {console.log("trades",trades)}
+        {console.log("post",resf)}
+            {/* {resf.map((post,key) =>{
+            return(
+                
+                <div key={key} style={{display:'flex',flexDirection:'row'}}><p>{post.datetime}</p>
+                <p style={{paddingRight:10}}>{post.side}</p>
+                <p style={{paddingRight:10}}>{post.price}</p>
+                <p style={{paddingRight:10}}>1.0</p>
+                <p style={{paddingRight:10}}>{post.cost}</p>
+                <p style={{paddingRight:10}}>{post.id}</p>
+                </div>
+        
+        )
+    })}
+       */}
       
             <div className='flex_box_table'>
                <h3>Trades</h3>
@@ -31,20 +55,22 @@ function Trades() {
         <table>
             <thead>
                 <th>Date</th>
-                <th>To</th>
-                <th>From</th>
-                <th>Gass Used</th>
-                <th>Total Price</th>
+                <th>Type</th>
+                <th>Price USD</th>
+                <th>Amount USD</th>
+                <th>Total ETH</th>
+                <th>Marker</th>
             </thead>
             <tbody>         
-                {trades.slice(0,10).map((post,key) =>{
+                {resf.slice(0,10).map((post,key) =>{
                 return(
                     <tr key={key}>
-                        <td>{moment.unix(post?.timeStamp).format("MMMM Do YYYY, h:mma")}</td>
-                        <td>{post?.to.slice(0, 17)}</td>
-                        <td>{post?.from.slice(0, 17)}</td>
-                        <td>${post?.gas}</td>
-                        <td>${post?.gasPrice.slice(0,-5)}</td>
+                        <td>{post.datetime}</td>
+                        <td>{post.side}</td>
+                        <td>${post.price}</td>
+                        <td>${post.amount}</td>
+                        <td>{post.cost}</td>
+                        <td>{post.id}</td>
                     </tr>
                     )
                 })}
