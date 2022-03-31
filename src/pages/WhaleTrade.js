c645f44b15a0c51190c5bc2cef6618b7fc0415e3
admin-logochemist <team@logochemist.com>
2022-04-01 03:15:03 +0500

    hamza

--- src/pages/WhaleTrade.js
+++ src/pages/WhaleTrade.js
@@ -9,13 +9,13 @@ function WhaleTrade() {
           <section className='bannerAdds_whale_trade'></section>
           <div className='container-fluid'>
             <div className='row'>
-                <div className='col-12'>
+                <div className='col-12 mt-3'>
                   <PriceTickers />
                 </div>
             </div>
             <div className='col-12'>
                   <CoinsTable/>
-                </div>
+            </div>
           </div>
           <DashFooter/>
          
