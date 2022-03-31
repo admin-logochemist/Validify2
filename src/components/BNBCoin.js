f6328945ab69d58d9746cdd3befde42b97475144
Taichee Rixvi <85700339+admin-logochemist@users.noreply.github.com>
2022-04-01 04:59:44 +0500

    Ghayas

--- src/components/BNBCoin.js
+++ src/components/BNBCoin.js
@@ -8,7 +8,7 @@ function Iframe(props) {
     );
   }
 function TetherCoin() {
-    const iframe = '<iframe id="tradingview_1bbc4" src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_1bbc4&amp;symbol=BNBUSDT&amp;interval=D&amp;hidesidetoolbar=1&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=F1F3F6&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=Dark&amp;style=1&amp;timezone=Etc%2FUTC&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;locale=en&amp;utm_source=validefi.global&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=BNBUSDT" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen=""></iframe>';
+    const iframe = '<iframe id="tradingview_f879f" src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_f879f&amp;symbol=BNBUSDT&amp;interval=D&amp;hidesidetoolbar=0&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=F1F3F6&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=Light&amp;style=1&amp;timezone=Etc%2FUTC&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;locale=en&amp;utm_source=coinmarketcap.com&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=BNBUSDT" style="width: 100%; margin: 0 !important; padding: 0 !important;" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen=""></iframe>';
     return (
             <div className='col_iframe scd_width'>
                 <h3 className='trandingview'>VALIDEX Charting</h3>
