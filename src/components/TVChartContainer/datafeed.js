a5240c7cb962f1e6d5003da9c6830e4e519a168c
ghayas ali <team@logochemist.com>
2022-04-12 22:38:54 +0500

    Ghayas

--- src/components/TVChartContainer/datafeed.js
+++ src/components/TVChartContainer/datafeed.js
@@ -1,140 +1,132 @@
 /* eslint-disable import/no-anonymous-default-export */
-import axios from "axios";
-import * as Bitquery from "./Bitquery";
+import axios from 'axios'; 
+import * as Bitquery from './Bitquery';
 
-const lastBarsCache = new Map();
+const lastBarsCache = new Map(); 
 const configurationData = {
-  supported_resolutions: ["1", "5", "15", "30", "60", "1D", "1W", "1M"],
-};
+    supported_resolutions: ['1','5','15','30', '60','1D', '1W', '1M']
+}; 
 export default {
-  // This method is used by the Charting Library to get a configuration of your datafeed
-  // (e.g. supported resolutions, exchanges and so on)
-  onReady: (callback) => {
-    console.log("[onReady]: Method called!!");
-    setTimeout(() => callback(configurationData));
-  },
-  resolveSymbol: async (
-    symbolName,
-    onSymbolResolvedCallback,
-    onResolveErrorCallback
-  ) => {
-    console.log("[resolveSymbol]: Method called!!");
-    let baseQuery = await localStorage.getItem("@baseQuery");
-    const response = await fetch(Bitquery.endpoint, {
-      method: "POST",
-      variables: {
-        tokenAddress: symbolName,
-      },
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        "X-API-KEY": `BQYH5kJPfhPiJsmS36s6zasFiNkfgWbD`,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-        Authorization: "Bearer <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>",
-      },
-      //body: JSON.stringify({ query: Bitquery.GET_COIN_INFO2(baseQuery) }) // ({ QUERY })
-      body: JSON.stringify({ query: Bitquery.GET_COIN_INFO(baseQuery) }), // ({ QUERY })
-    })
-      .then((response) => {
-        if (response.status >= 400) {
-          throw new Error("Error fetching data");
-        } else {
-          return response.json();
-        }
-      })
-      .then(
-        (data) => (window.mydata = data.data.ethereum.dexTrades[0].baseCurrency)
-      );
-    // const coin = response.data.data.ethereum.dexTrades[0].baseCurrency;
-    // console.log(response.data.data.ethereum.dexTrades[0].quotePrice);
-    //console.log(response.data.data.ethereum.dexTrades[0].baseCurrency);
-    console.log("query Posted");
-    const coin = window.mydata;
-    if (!coin) {
-      onResolveErrorCallback();
-    } else {
-      const symbol = {
-        ticker: symbolName,
-        name: `${coin.symbol}/WETH`,
-        session: "24x7",
-        timezone: "Etc/UTC",
-        minmov: 1,
-        pricescale: 10000000,
-        has_intraday: true,
-        intraday_multipliers: ["1", "5", "15", "30", "60"],
-        has_empty_bars: true,
-        has_weekly_and_monthly: false,
-        supported_resolutions: configurationData.supported_resolutions,
-        volume_precision: 1,
-        data_status: "streaming",
-      };
-      //onSymbolResolvedCallback(symbol);
-      onSymbolResolvedCallback(symbol);
-    }
-  },
-  getBars: async (
-    symbolInfo,
-    resolution,
-    periodParams,
-    onHistoryCallback,
-    onErrorCallback,
-    first
-  ) => {
-    const { from, to, firstDataRequest } = periodParams;
-    console.log("[getBars]: Method call", symbolInfo, resolution, from, to);
-    let baseQuery = await localStorage.getItem("@baseQuery");
-    try {
-      if (resolution === "15m") {
-        resolution = 1440;
-      }
-      const response2 = await fetch(Bitquery.endpoint, {
-        method: "POST",
-        variables: {
-          from: new Date("2021-06-20T07:23:21.000Z").toISOString(),
-          to: new Date("2021-06-23T15:23:21.000Z").toISOString(),
-          interval: Number(resolution),
-          tokenAddress: symbolInfo.ticker,
-        },
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-          "X-API-KEY": "BQYH5kJPfhPiJsmS36s6zasFiNkfgWbD",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
-        body: JSON.stringify({ query: Bitquery.GET_COIN_BARS(baseQuery) }),
-      })
+    // This method is used by the Charting Library to get a configuration of your datafeed 
+    // (e.g. supported resolutions, exchanges and so on)
+    onReady: (callback) => {
+        console.log('[onReady]: Method called!!');
+        setTimeout(() => callback(configurationData));
+    },
+    resolveSymbol: async (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) =>{
+        console.log('[resolveSymbol]: Method called!!'); 
+        let baseQuery =  localStorage.getItem('@baseQuery')
+        const response = await fetch(
+            Bitquery.endpoint, {
+                method: "POST",
+                variables: {
+                    "tokenAddress": symbolName
+                },
+                mode: 'cors',
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-API-KEY": `BQYH5kJPfhPiJsmS36s6zasFiNkfgWbD`,
+                    "Access-Control-Allow-Origin" : "*", 
+                    "Access-Control-Allow-Credentials" : true,
+                    'Authorization' : 'Bearer <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>'
+                },
+                //body: JSON.stringify({ query: Bitquery.GET_COIN_INFO2(baseQuery) }) // ({ QUERY })
+                 body: JSON.stringify({ query: Bitquery.GET_COIN_INFO(baseQuery) }) // ({ QUERY })
+            
+            }
+        )
         .then((response) => {
-          if (response.status >= 400) {
-            throw new Error("Error fetching data");
-          } else {
-            return response.json();
-          }
-        })
-        .then(
-          (data) =>
-            (window.bars = data.data.ethereum.dexTrades.map((el) => ({
-              time: new Date(el.timeInterval.minute).getTime(), // date string in api response
-              low: el.low,
-              high: el.high,
-              open: Number(el.open),
-              close: Number(el.close),
-              volume: el.volume,
-            })))
-        );
-      if (firstDataRequest) {
-        lastBarsCache.set(symbolInfo.full_name, {
-          ...window.bars[window.bars.length - 1],
-        });
-      }
+            if (response.status >= 400) {
+              throw new Error("Error fetching data");
+            } else {
+              return response.json();
+            }
+          })
+          .then((data) => window.mydata=data.data.ethereum.dexTrades[0].baseCurrency);
+        // const coin = response.data.data.ethereum.dexTrades[0].baseCurrency; 
+        // console.log(response.data.data.ethereum.dexTrades[0].quotePrice); 
+        //console.log(response.data.data.ethereum.dexTrades[0].baseCurrency); 
+    console.log("query Posted")
+        const coin = window.mydata; 
+        if(!coin){
+            onResolveErrorCallback(); 
+        }else{
+            const symbol = {
+                ticker: symbolName,
+                name: `${coin.symbol}/WETH`,
+                session: '24x7',
+                timezone: 'Etc/UTC',
+                minmov: 1,
+                pricescale: 10000000,
+                has_intraday: true,
+                intraday_multipliers: ['1', '5', '15', '30', '60'],
+                has_empty_bars: true,
+                has_weekly_and_monthly: false,
+                supported_resolutions: configurationData.supported_resolutions, 
+                volume_precision: 1,
+                data_status: 'streaming',
+            }
+            //onSymbolResolvedCallback(symbol);
+            onSymbolResolvedCallback(symbol) 
+        }
+    }, 
+    getBars: async(symbolInfo, resolution,periodParams, onHistoryCallback, onErrorCallback, first,) =>{
+        const { from, to, firstDataRequest } = periodParams;
+        console.log('[getBars]: Method call', symbolInfo, resolution, from, to);
+        let baseQuery =  localStorage.getItem('@baseQuery')
+        try{
+            if (resolution==='15m') {
+                resolution = 1440;
+            }
+            const response2 = await fetch(Bitquery.endpoint, {
+                method: "POST",
+                variables: {
+                    "from": new Date("2021-06-20T07:23:21.000Z").toISOString(),
+                    "to": new Date("2021-06-23T15:23:21.000Z").toISOString(),
+                    "interval": Number(resolution),
+                    "tokenAddress": symbolInfo.ticker
+                },
+                mode: 'cors',
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-API-KEY": "BQYH5kJPfhPiJsmS36s6zasFiNkfgWbD",
+                    "Access-Control-Allow-Origin" : "*", 
+                    "Access-Control-Allow-Credentials" : true,
+                },
+                body: JSON.stringify({ query: Bitquery.GET_COIN_BARS(baseQuery) })
+            
+            })
+            .then((response) => {
+                if (response.status >= 400) {
+                  throw new Error("Error fetching data");
+                } else {
+                  return response.json();
+                }
+              })
+              .then((data) => window.bars=data.data.ethereum.dexTrades.map(el => ({
+                time: new Date(el.timeInterval.minute).getTime(), // date string in api response
+                low: el.low,
+                high: el.high,
+                open: Number(el.open),
+                close: Number(el.close),
+                volume: el.volume
+            }))
+           
+            )
+          if (firstDataRequest) {
+				lastBarsCache.set(symbolInfo.full_name, {
+					...window.bars[window.bars.length - 1],
+				});
+			}
+			
+			onHistoryCallback(window.bars, {
+				noData: false,
+			});
+        } catch(err){
+            console.log({err})
+            // onErrorCallback(err)
+        }
+    },
 
-      onHistoryCallback(window.bars, {
-        noData: false,
-      });
-    } catch (err) {
-      console.log({ err });
-      // onErrorCallback(err)
-    }
-  },
-};
+
+}
--- src/pages/SearchTokenData.js
+++ src/pages/SearchTokenData.js
@@ -1,104 +1,77 @@
-import React, { useState, useEffect } from "react";
-import "../css/WhaleTrades.css";
-import "../css/trades.css";
-import Ethcoin_icon from "../images/eth.svg";
-import PriceTickers from "../components/PriceTickers";
-import DashFooter from "../components/DashFooter";
-import { TVChartContainer } from "../components/TVChartContainer";
+import React,{useState,useEffect} from 'react'
+import '../css/WhaleTrades.css'
+import '../css/trades.css'
+import Ethcoin_icon from '../images/eth.svg'
+import PriceTickers from '../components/PriceTickers'
+import DashFooter from '../components/DashFooter'
+import { TVChartContainer } from '../components/TVChartContainer'
 function SearchTokenData() {
-  const [resd, setResd] = useState([]);
-  const [search, setSearch] = useState();
-  useEffect(() => {
-    callApi();
-  }, [search]);
-  const callApi = () => {
-    if (search) {
-      fetch(`https://validefi.global:8080/exchange?bcurrency=${search}`).then(
-        (resd) =>
-          resd.json().then((re) => {
-            setResd(re);
-          })
-      );
-    } else {
+    const[resd,setResd]=useState([]);
+    const [search,setSearch]=useState()
+    useEffect(() => {
+        callApi()
+    }, [search])
+    const callApi=()=>{
+        if(search){
+
+            fetch(`https://validefi.global:8080/exchange?bcurrency=${search}`).then((resd)=>resd.json().then(re=>{setResd(re)
+        }))
+}else{
+
+}
+
+
     }
-  };
-  return (
-    <>
-      <section className="bannerAdds_whale_trade"></section>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 mt-3">
-            <PriceTickers />
-          </div>
-        </div>
-        <div className="col-12">
-          <div className="table_trades">
-            <div className="Trades_search">
-              <input
-                type="text"
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                  localStorage
-                    .setItem("@baseQuery", e.target.value)
-                    .then(() => {
-                      console.log("saved to storage");
-                    });
-                }}
-                placeholder="Search Token Address"
-              />
-              <button
-                type="submit"
-                onClick={() => {
-                  callApi();
-                }}
-              >
-                Search
-              </button>
-            </div>
-            <div className="icons_info">
-              {resd.slice(0, 1).map((post, key) => {
-                return (
-                  <div className="img_box">
-                    <img src={Ethcoin_icon} />
-                    <li className="wrapper_name">
-                      {post.quoteCurrency.symbol} / {post.baseCurrency.symbol}{" "}
-                    </li>
-                  </div>
-                );
-              })}
-              {resd.slice(0, 1).map((post, key) => {
-                return (
-                  <div className="coin_names">
-                    <li className="value_names">
-                      <li>{post.quoteAmount}</li>
-                    </li>
-                    <span>ETH {post.quotePrice}</span>
-                  </div>
-                );
-              })}
-            </div>
-            {resd.slice(0, 1).map((post, key) => {
-              return (
-                <div className="li_pairs">
-                  <li className="li_div">
-                    {post.baseCurrency.name}
-                    <li className="inner_li">
-                      Token:{post.baseCurrency.address}
-                    </li>
-                    <li className="inner_li">
-                      Pair: {post.quoteCurrency.address}
-                    </li>
-                  </li>
+    return (
+        <>
+             <section className='bannerAdds_whale_trade'></section>
+          <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-12 mt-3'>
+                  <PriceTickers />
                 </div>
-              );
-            })}
-            {console.log("post", resd)}
-            <div id="tv_chart_container">
-              <TVChartContainer baseQuery={search} />
             </div>
-            <div className="flex_box_table">
-              <h3>Trades</h3>
-              {/* <div className='btns_lengends'>
+            <div className='col-12'>
+            <div className='table_trades'>
+                <div className='Trades_search'>
+                    <input type="text" onChange={(e)=>{setSearch(e.target.value);localStorage.setItem('@baseQuery',e.target.value).then(()=>{
+                    console.log('saved to storage');
+                    })}} placeholder='Search Token Address'/>
+                    <button type="submit" onClick={()=>{callApi()}}>Search</button>
+                </div>
+                <div className='icons_info'>
+                {resd.slice(0,1).map((post,key) =>{
+                        return(
+                    <div className='img_box'>
+                        <img src={Ethcoin_icon}/>
+                        <li className='wrapper_name'>{post.quoteCurrency.symbol} / {post.baseCurrency.symbol} </li>
+                    </div>
+                  )})}
+                   {resd.slice(0,1).map((post,key) =>{
+                        return(
+                    <div className='coin_names'>
+                        <li className='value_names'><li>{post.quoteAmount}</li></li>
+                        <span>ETH {post.quotePrice}</span>
+                    </div>
+                        )})}
+                </div>
+                {resd.slice(0,1).map((post,key) =>{
+                        return(
+                <div className='li_pairs'>
+                    <li className='li_div'>
+                    {post.baseCurrency.name}
+                        <li className='inner_li'>Token:{post.baseCurrency.address}</li>
+                        <li className='inner_li'>Pair: {post.quoteCurrency.address}</li>
+                    </li>
+                </div>
+                  )})}
+              {console.log("post",resd)}  
+              <div id="tv_chart_container">
+<TVChartContainer baseQuery={search}/>
+    </div>
+            <div className='flex_box_table'>
+               <h3>Trades</h3>
+               {/* <div className='btns_lengends'>
                     <button className='buy'>Buy</button>
                     <button className='sell'>Sale</button>
                </div> */}
@@ -106,45 +79,40 @@ function SearchTokenData() {
 
             {/* ++_-_++  TRADES TABLE DATA  ++_-_++ */}
             <table>
-              <thead>
-                <th>Date</th>
-                <th>Type</th>
-                <th>PRICE USDT</th>
-                <th className="transform">Price ETH</th>
-                <th className="transform">Amount WETH</th>
-                <th className="transform">TOTAL ETH</th>
-                <th>Maker</th>
-              </thead>
-              <tbody>
-                {resd.slice(0, 10).map((post, key) => {
-                  // if(post.side=="buy"){
+                <thead>
+                      <th>Date</th>
+                    <th>Type</th>
+                    <th>PRICE USDT</th>           
+                    <th className='transform'>Price ETH</th>
+                    <th className='transform'>Amount WETH</th>
+                    <th className='transform'>TOTAL ETH</th>
+                    <th>Maker</th>
+                </thead>
+                <tbody>         
+                    {resd.slice(0,10).map((post,key) =>{
+                        // if(post.side=="buy"){
 
-                  return (
-                    <tr key={key}>
-                      <td className="date_table">
-                        {post.block.timestamp.iso8601
-                          .replace("T", "..")
-                          .slice(0, -4)}
-                      </td>
-                      <td className="red">{post.side}</td>
-                      <td className="">{post.baseAmount}</td>
-                      <td className="truncate">{post.quoteAmount}</td>
-                      <td className="truncate">{post.quotePrice}</td>
-                      <td className="truncate">{post.baseAmount}</td>
-                      <td className="truncate maker_table">
-                        {post.maker.address.slice(0, -2)}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                        
+                    return(
+                        <tr key={key}>
+                            <td className='date_table'>{(post.block.timestamp.iso8601).replace('T','..').slice(0, -4)}</td>
+                            <td className='red'>{post.side}</td>
+                            <td className=''>{post.baseAmount}</td>                  
+                            <td className='truncate'>{post.quoteAmount}</td>
+                            <td className='truncate'>{post.quotePrice}</td>
+                            <td className='truncate'>{(post.baseAmount)}</td>
+                            <td className='truncate maker_table'>{(post.maker.address).slice(0, -2)}</td>
+                        </tr>
+                      
+                    )})}
+            </tbody>
+            </table>        
+    </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <DashFooter />
-    </>
-  );
+          <DashFooter/>
+        </>
+    )
 }
 
-export default SearchTokenData;
+export default SearchTokenData
