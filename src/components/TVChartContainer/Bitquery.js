d5e9aa39cc1e680a5f46eccc7f0819abfa92da08
ghayas ali <team@logochemist.com>
2022-04-19 23:56:26 +0500

    Ghayas

--- src/components/TVChartContainer/Bitquery.js
+++ src/components/TVChartContainer/Bitquery.js
@@ -7,8 +7,8 @@ export const GET_COIN_INFO=(baseQuery,qQuery,network,exchange)=>{
     dexTrades(
       options: {desc: ["block.height", "transaction.index"], limit: 1}
       exchangeName: {is: "${exchange}"},
-      quoteCurrency: {is: "${qQuery}"},
-      baseCurrency: {is: "${baseQuery}"}
+      quoteCurrency: {is: "${baseQuery}"},
+      baseCurrency: {is: "${qQuery}"}
     ) {
       block {
         height
@@ -33,41 +33,60 @@ export const GET_COIN_INFO=(baseQuery,qQuery,network,exchange)=>{
 return myQuery;
 }
 
-export const GET_COIN_BARS =(baseQuery,qQuery,network,exchange,from)=>{
+export const GET_COIN_BARS =(baseQuery,qQuery,network,exchange)=>{
   let myQuery;
+  if(network==='ethereum'){
     myQuery =`
     {
-      ethereum(network: ${network}) {
-        dexTrades(
-          options: {limit: 100000, asc: "timeInterval.hour"}
-          date: {since: "2021-06-20T07:23:21.000Z"}
-          baseCurrency: {is: "${baseQuery}"}
-          quoteCurrency: {is: "${qQuery}"}
-        ) {
-          timeInterval {
-            hour(count: 4)
-          }
-          baseCurrency {
-            symbol
-            address
-          }
-          baseAmount
-          quoteCurrency {
-            symbol
-            address
-          }
-          quoteAmount
-          trades: count
-          quotePrice
-          maximum_price: quotePrice(calculate: maximum)
-          minimum_price: quotePrice(calculate: minimum)
-          open_price: minimum(of: block, get: quote_price)
-          close_price: maximum(of: block, get: quote_price)
-        }
-      }
-    }
+     ethereum(network: ${network}) {
+       dexTrades(
+         options: {asc: "timeInterval.minute", limit:1000}
+         date: {since: "2021-06-20" till: "2022-04-19"}
+         exchangeName: {is: "${exchange}"}
+         quoteCurrency: {is: "${baseQuery}"}
+         baseCurrency: {is: "${qQuery}"}
+         
+       ) {
+         timeInterval {
+           minute(count: 5, format: "%Y-%m-%dT%H:%M:%SZ")
+         }
+         volume: quoteAmount
+         high: quotePrice(calculate: maximum)
+         low: quotePrice(calculate: minimum)
+         open: minimum(of: block, get: quote_price)
+         close: maximum(of: block, get: quote_price)
+       }
+     }
+   }
+   
    `; 
-  
+  }else{
+    console.log("bnb taiche");
+    myQuery =`
+    {
+     ethereum(network: ${network}) {
+       dexTrades(
+         options: {asc: "timeInterval.minute" }
+         date: {is: "2022-01-20" }
+         exchangeName: {is: "${exchange}"}
+         quoteCurrency: {is: "${qQuery}"}
+         baseCurrency: {is: "${baseQuery}"}
+         tradeAmountUsd: {gt: 10}
+       ) {
+         timeInterval {
+           minute(count: 1, format: "%Y-%m-%dT%H:%M:%SZ")
+         }
+         volume: quoteAmount
+         high: quotePrice(calculate: maximum)
+         low: quotePrice(calculate: minimum)
+         open: minimum(of: block, get: quote_price)
+         close: maximum(of: block, get: quote_price)
+       }
+     }
+   }
+   
+   `;
+  }
   
 
  
